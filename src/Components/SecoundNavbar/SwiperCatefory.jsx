d348b1658bed1c0d54b8a8a3a0d96f40a594bbf8
John Doe <mohaiminur24@gmail.com>
2023-10-04 12:30:22 +0600

    secound navbar fully complete

--- src/Components/SecoundNavbar/SwiperCatefory.jsx
+++ src/Components/SecoundNavbar/SwiperCatefory.jsx
@@ -3,24 +3,101 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Navigation } from "swiper/modules";
+import { BiRestaurant, BiSolidCity, BiBuildings } from "react-icons/bi";
+import { GiVillage, GiWorld } from "react-icons/gi";
+import { TbBeachOff } from "react-icons/tb";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 const SwiperCatefory = () => {
+  const [category, setCategory] = useState("all");
+
   return (
-    <div>
+    <div className="flex justify-center items-center gap-5">
+      <button className="prev p-2 shadow-sm border rounded-full">
+        <MdKeyboardArrowLeft />
+      </button>
       <Swiper
-        slidesPerView={3}
+        slidesPerView={4}
         spaceBetween={30}
-        navigation={true}
+        navigation={{
+          prevEl: ".prev",
+          nextEl: ".next",
+        }}
         modules={[Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("all")}
+            className={`flex flex-col justify-center items-center ${
+              category == "all" && " pb-1 border-gray-800 border-b-2"
+            }`}
+          >
+            <BiRestaurant className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">All Restaurant</span>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("iconiccities")}
+            className={`flex flex-col justify-center items-center ${
+              category == "iconiccities" && "border-b-2 pb-1 border-black"
+            }`}
+          >
+            <BiSolidCity className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">Iconic Cities</span>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("countryside")}
+            className={`flex flex-col justify-center items-center ${
+              category == "countryside" && "border-b-2 pb-1 border-black"
+            }`}
+          >
+            <GiVillage className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">Country Sides</span>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("topoftheworld")}
+            className={`flex flex-col justify-center items-center ${
+              category == "topoftheworld" && "border-b-2 pb-1 border-black"
+            }`}
+          >
+            <GiWorld className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">
+              Top of the World
+            </span>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("beach")}
+            className={`flex flex-col justify-center items-center ${
+              category == "beach" && "border-b-2 pb-1 border-black"
+            }`}
+          >
+            <TbBeachOff className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">Beach</span>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <button
+            onClick={() => setCategory("luxe")}
+            className={`flex flex-col justify-center items-center ${
+              category == "luxe" && "border-b-2 pb-1 border-gray-800"
+            }`}
+          >
+            <BiBuildings className="text-3xl" />
+            <span className="tracking-tighter font-Mooli">Luxe</span>
+          </button>
+        </SwiperSlide>
       </Swiper>
+      <button className="next p-2 shadow-sm border rounded-full">
+        <MdKeyboardArrowRight />
+      </button>
     </div>
   );
 };
