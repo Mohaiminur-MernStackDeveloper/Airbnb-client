273fe03d4c2760ffcef7467321ccdf2b8816dfed
John Doe <mohaiminur24@gmail.com>
2023-10-05 17:35:43 +0600

    single card design is complete

--- src/Components/DataBody/Resturentcard.jsx
+++ src/Components/DataBody/Resturentcard.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchresturent } from '../../Redux/Features/Resturent/ResturentData';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchresturent } from "../../Redux/Features/Resturent/ResturentData";
+import ResturentSingleCard from "./ResturentSingleCard";
 
 const Resturentcard = () => {
-    const {resturetrentData} = useSelector((state)=>state.resturentSlice);
-    const dispatch = useDispatch();
+  const { resturetrentData } = useSelector((state) => state.resturentSlice);
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch(fetchresturent());
-    },[])
+  useEffect(() => {
+    dispatch(fetchresturent());
+  }, []);
 
-
-    console.log(resturetrentData);
-
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-11/12 mx-auto grid grid-cols-5 gap-5 justify-center items-start">
+      {resturetrentData &&
+        resturetrentData.map((data, index) => (
+          <ResturentSingleCard key={index} data={data} />
+        ))}
+    </div>
+  );
 };
 
 export default Resturentcard;
